8488016308b8425ffd3787d1ac37334f4e7a1742
lahiruSanjeewa2971 <rathne1997@gmail.com>
2025-03-01 21:58:54 +0530

    Start backend API

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,32 @@
-import express from 'express';
-const app = express()
+import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import DeckModel from "./models/Deck.js";
 
-app.get("/", (req, res) => {
-    res.send("Jo - 1")
-})
+const PORT = 5000;
 
-app.listen(5000)
+// Load environment variables
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.post("/deck", async (req: Request, res: Response) => {
+    console.log('hi')
+  const newDeck = new DeckModel({
+    title: "First Title of the First Deck.",
+  });
+  const savedResponse = await newDeck.save();
+  console.log("hi hi :", savedResponse)
+  res.send(savedResponse);
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://rathne1997:0718003955@typecriptprojects.jtlmn.mongodb.net/notes_app?retryWrites=true&w=majority&appName=typecriptProjects"
+  )
+  .then(() => {
+    console.log(`Connected to the DB | port : ${PORT}`);
+    app.listen(PORT);
+  });
